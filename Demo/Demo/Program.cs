513f43f4943eef83e72bd33436f36b93f1d213a6
Yuriy Nicheporuk <yuriy.nicheporuk@Blackthorn-vision.com>
2021-10-26 13:57:57 +0300

    Created project

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Generator
+{
+    class Program
+    {
+        const string stopKey = "n";
+        static readonly Random random = new();
+
+        static void Main(string[] args)
+        {
+            bool stopEatingCake = false;
+            do
+            {
+                var userReponse = GetUserReponse();
+
+                stopEatingCake = userReponse == stopKey;
+
+                if (!stopEatingCake)
+                {
+                    var number = random.Next(1, 100);
+
+                    if (number < 50)
+                    {
+                        Console.WriteLine("App thinks that you have already eaten enough cake for today!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Let's eat one more piece!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Completed for now");
+                }
+            }
+            while (!stopEatingCake);
+        }
+
+        static string GetUserReponse()
+        {
+            Console.WriteLine($"Would you like to know if you need one extra piece of cake? Press any key " +
+                $"to continue or '{stopKey}' to stop");
+
+            var result = Console.ReadLine();
+
+            return result;
+        }
+    }
+}
